2fb09cff46cc66a6ecb569727303e6b25d541800
Niko Niyazi Türk <71526347+NikoNiyazi@users.noreply.github.com>
2022-03-12 22:34:06 +0300

    Creation

---
+++ functions.lua
@@ -0,0 +1,39 @@
+--[[
+             ADV PRINT - PRINT EASIER AND MORE ADVANCED!
+             MADE BY RealTurkishCat
+             NIKOO#6661
+]]
+
+--FUNCTIONS
+function labelprint(lparent, text, size)
+	local label = Instance.new("TextLabel")
+	label.Name = "LabelPrint_from_advprint"
+	label.Parent = lparent
+	label.Size = size
+	label.Text = text
+end
+
+function waitprint(txt,t)
+	wait(t)
+	print(txt)
+end
+
+function plradded()
+	game.Players.PlayerAdded:Connect(function(player)
+		print(player.Name.."joined the game!")
+	end)
+end
+
+function plrremoved()
+	game.Players.PlayerRemoving:Connect(function(player)
+		print(player.Name.."left the game!")
+	end)
+end
+
+function wprint(text, delaytime)
+	while true do
+		print(text)
+		wait(delaytime)
+	end
+end
+
